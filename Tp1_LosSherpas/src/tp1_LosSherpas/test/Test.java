0a58f3bc0d1a4aff652069061df36ebf6c81897c
Lisandro <Lisandro@192.168.1.91>
2020-08-23 11:26:01 -0300

    Mensaje Inicial!! a Trabajar mirar los videos

---
+++ Tp1_LosSherpas/src/tp1_LosSherpas/test/Test.java
@@ -0,0 +1,9 @@
+package tp1_LosSherpas.test;
+
+public class Test {
+	public static void main(String[]args) {
+		String cadena1 = "Hola";
+		String cadena2 = "Sherpas";
+		System.out.println(cadena1+" "+cadena2);
+	}
+}
